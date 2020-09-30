2b94a8730ff967e5b16f3485e31f71603b23e1a5
vityajulin <vkdvornikov@gmail.com>
2020-09-30 19:45:29 +0300

    refactor post time to joba.time

--- src/model/PostModel.kt
+++ src/model/PostModel.kt
@@ -1,17 +1,18 @@
 package com.example.model
+import org.joda.time.LocalDate
 
 data class PostModel(
-    val id: Long,
-    val sourceId: Long? = null,
-    val ownerId: Long,
-    val content: String? = null,
-    val created: Int = (System.currentTimeMillis() / 1000).toInt(),
-    val likes: Set<Long> = setOf(),
-    val dislikes: Set<Long> = setOf(),
-    val reposts: Map<Long, Long> = mapOf(),
-    val link: String? = null,
-    val type: PostType = PostType.POST,
-    val attachment: AttachmentModel?
+        val id: Long,
+        val sourceId: Long? = null,
+        val ownerId: Long,
+        val content: String? = null,
+        val created: LocalDate = LocalDate(),
+        val likes: Set<Long> = setOf(),
+        val dislikes: Set<Long> = setOf(),
+        val reposts: Map<Long, Long> = mapOf(),
+        val link: String? = null,
+        val type: PostType = PostType.POST,
+        val attachment: AttachmentModel?
 )
 
 enum class PostType {
